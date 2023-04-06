dac41234135cb2a4954a9988feabb96c82cd4de1
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-04-06 12:58:02 +0300

    updates

--- valuation_multiple/valuation_multiple/settings.py
+++ valuation_multiple/valuation_multiple/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'stock_analysis.apps.StockAnalysisConfig',
 ]
 
 MIDDLEWARE = [
