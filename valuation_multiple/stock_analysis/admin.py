c78db720767fb228a8a80d0c1e88f9d206e26ca5
mxstrv <37843972+mxstrv@users.noreply.github.com>
2023-04-12 10:42:02 +0300

    fixes

--- valuation_multiple/stock_analysis/admin.py
+++ valuation_multiple/stock_analysis/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from stock_analysis.models.stock_model import Stock
+from stock_analysis.models.fundamental_analysis_model import FundamentalAnalysis
 
-# Register your models here.
+admin.site.register(FundamentalAnalysis)
+admin.site.register(Stock)
--- valuation_multiple/stock_analysis/urls.py
+++ valuation_multiple/stock_analysis/urls.py
@@ -5,7 +5,7 @@ from stock_analysis.views.stocks_view import (
     new_stock,
     create_stock,
 )
-from stock_report.views.fundamental_analysis_view import (
+from stock_analysis.views.fundamental_analysis_view import (
     IndexView,
     detail,
     new_fundamental_analysis,
@@ -14,25 +14,16 @@ from stock_report.views.fundamental_analysis_view import (
 )
 
 
-app_name = "stock_reports"
+app_name = "stock_analysis"
 urlpatterns = [
-    # ex: /stock_report/
     path("", IndexView.as_view(), name="index"),
-    # ex: /stock_report/1/
     path("<int:fundamental_analysis_id>/", detail, name="detail"),
-    # ex: /stock_report/new/
     path("new/", new_fundamental_analysis, name="new"),
-    # ex: POST /stock_report/create/
     path("create/", create_fundamental_analysis, name="create"),
-    # ex: /stock_report/stock/new/
     path("stock/new/", new_stock, name="new_stock"),
-    # ex: POST /stock_report/stock/create/
     path("stock/create/", create_stock, name="create_stock"),
-    # ex: /stock_report/stock/1/
     path("stock/<int:stock_id>/", stock_detail, name="stock_detail"),
-    # ex: POST /stock_report/stock/1
     path("<int:fundamental_analysis_id>", add_stock, name="add_stock"),
-    # ex: /stock_report/delete_stock_from_fundamental_analysis/1/1
     path(
         "<int:fundamental_analysis_id>/<int:stock_id>",
         delete_stock_from_fundamental_analysis,
